38a4d6fdbe7da58bb867629807b09e4dbb0dc308
‘jayce8887@gmail.com’ <kjw88887@gmail.com>
2024-06-15 16:36:24 +0900

    오류핸들링 및 가입,로그인시 비밀번호 평문으로 변경

--- src/main/java/com/codecrafter/commenting/domain/request/ProfileRequest.java
+++ src/main/java/com/codecrafter/commenting/domain/request/ProfileRequest.java
@@ -1,14 +1,27 @@
 package com.codecrafter.commenting.domain.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "프로필 요청")
 public record ProfileRequest(
+    @Schema(description = "이메일", example = "jayce@crafter.com")
     String email,
+    @Schema(description = "닉네임", example = "제이스")
     String nickname,
+    @Schema(description = "소개", example = "난 제이스야")
     String introduce,
+    @Schema(description = "링크1", example = "/인스타그램주소/")
     String link1,
+    @Schema(description = "링크2", example = "/블로그1주소/")
     String link2,
+    @Schema(description = "링크3", example = "/블로그2주소/")
     String link3,
+    @Schema(description = "썸네일", example = "/프로필이미지경로/")
     String avatarPath,
+    @Schema(description = "익명여부", example = "true")
     Boolean allowAnonymous,
+
+    @Schema(description = "메일수신여부", example = "true")
     Boolean emailNotice,
     String token
 ) {
--- src/main/java/com/codecrafter/commenting/swagger/SwaggerConfig.java
+++ src/main/java/com/codecrafter/commenting/swagger/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package com.codecrafter.commenting.swagger;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,6 +17,7 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI swaggerApi() {
+        //스웨거 JWT 설정
         return new OpenAPI()
             .components(new Components()
                 .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
@@ -21,5 +26,13 @@ public class SwaggerConfig {
                     .scheme("bearer")
                     .bearerFormat("JWT")))
             .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
+    }
+    @Bean
+    public GroupedOpenApi customOpenApi() {
+        // 아래 경로 스캔하여 스웨거 범위 지정
+        return GroupedOpenApi.builder()
+                            .group("commenting")
+                            .packagesToScan("com.codecrafter.commenting")
+                            .build();
     }
 }
