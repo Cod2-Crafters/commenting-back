381051e91cf714ee9a0ab665bda8581f7927780f
‘jayce8887@gmail.com’ <kjw88887@gmail.com>
2024-06-16 15:55:05 +0900

    (가입)이메일 중복검사 추가

--- src/main/java/com/codecrafter/commenting/controller/MemberController.java
+++ src/main/java/com/codecrafter/commenting/controller/MemberController.java
@@ -8,6 +8,7 @@ import com.codecrafter.commenting.domain.response.SignInResponse;
 import com.codecrafter.commenting.domain.response.SignUpResponse;
 import com.codecrafter.commenting.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -64,10 +66,26 @@ public class MemberController {
 //        return null;
 //    }
 
+    @Operation(summary = "이메일 중복 검사",
+        description = """
+                        ★이메일 중복 검사</br>
+                        {host}/api/member/check-email
+                        """)
+    @GetMapping("/check-email")
+    public ResponseEntity<ApiResponse> checkEmailDuplicate(
+                    @Parameter(description = "이메일", example = "jayce@crafter.com") @RequestParam String email) {
+        try {
+            boolean isDup = memberService.chkDupEmail(email);
+            return ResponseEntity.ok(ApiResponse.success(isDup));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(ApiResponse.error(e.getMessage()));
+        }
+    }
+
     @GetMapping("/jwt-test")
     public String getUser(@AuthenticationPrincipal MemberAuth memberAuth) {
-        log.info("memberAuth : {}", memberAuth.getEmail());
-        log.info("memberAuth : {}", memberAuth.getId());
+        log.info("getUser memberAuth : {}", memberAuth.getEmail());
+        log.info("getUser memberAuth : {}", memberAuth.getId());
         return memberAuth.getEmail();
     }
 
