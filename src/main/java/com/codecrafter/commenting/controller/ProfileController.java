3f9d6eba13b5f293657d634fa78643f9452700fb
‘jayce8887@gmail.com’ <kjw88887@gmail.com>
2024-06-17 21:02:21 +0900

    add avatar upload

--- src/main/java/com/codecrafter/commenting/controller/ProfileController.java
+++ src/main/java/com/codecrafter/commenting/controller/ProfileController.java
@@ -1,5 +1,9 @@
 package com.codecrafter.commenting.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.codecrafter.commenting.domain.dto.ApiResponse;
@@ -7,14 +11,19 @@ import com.codecrafter.commenting.domain.request.ProfileRequest;
 import com.codecrafter.commenting.domain.response.ProfileResponse;
 import com.codecrafter.commenting.service.ProfileService;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/profile")
@@ -40,22 +49,23 @@ public class ProfileController {
         return ResponseEntity.ok(ApiResponse.success(profileResponse));
     }
 
-    @Operation(summary = "프로필 수정",
-        description = """
-                    ★프로필 수정</br>
-                    {host}/api/profile/profile/{}
-                    """)
-    @PutMapping("/profile/{id}")
-    public ResponseEntity<ApiResponse> updateProfile(@PathVariable Long id
-                                                    , @RequestBody ProfileRequest request
-                                                    , @RequestHeader("Authorization") String token) {
-        log.info("updateProfile id: {}", id);
-        log.info("updateProfile request: {}", request);
-        log.info("updateProfile token: {}", token);
-
+    @Operation(summary = "프로필 수정")
+    @PutMapping(value = "/profile/{id}")
+    public ResponseEntity<ApiResponse> updateProfile(
+                                                        @PathVariable Long id,
+                                                        @RequestPart("profile") @Parameter(description = "프로필 데이터", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)) ProfileRequest request,
+                                                        @RequestHeader("Authorization") String token
+    ) {
         ProfileResponse profileResponse = profileService.updateProfile(id, request, token);
-
         return ResponseEntity.ok(ApiResponse.success(profileResponse));
+    }
+
+
+    @PostMapping(path = "/profile/{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "프로필썸네일 파일 업로드")
+    public ResponseEntity<ApiResponse>  avatarUpload(@PathVariable Long id, @ModelAttribute @Valid MultipartFile avatar, @RequestHeader("Authorization") String token) {
+        String avatarUrl = profileService.uploadAvatarFile(id, avatar, token);
+        return ResponseEntity.ok(ApiResponse.success(avatarUrl));
     }
 
 }
