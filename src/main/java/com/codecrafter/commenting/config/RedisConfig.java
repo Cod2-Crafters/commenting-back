36b250ff84c032eed23fcdfd52ce8951d35d568f
‘jayce8887@gmail.com’ <kjw88887@gmail.com>
2024-07-07 04:26:38 +0900

    레디스 설정 변경

--- src/main/java/com/codecrafter/commenting/config/RedisConfig.java
+++ src/main/java/com/codecrafter/commenting/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package com.codecrafter.commenting.config;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,18 +20,25 @@ public class RedisConfig {
     private String host;
     @Value("${spring.data.redis.mail.port}")
     private int port;
+    @Value("${spring.data.redis.mail.password}")
+    private String password;
 
     @Bean
-    public RedisConnectionFactory redisMailConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public RedisClient redisClient() {
+        RedisURI redisUri = RedisURI.Builder.redis(host, port)
+            .withSsl(true)
+            .withPassword(password)
+            .build();
+        return RedisClient.create(redisUri);
     }
 
-    @Bean(name = "redisTemplate")
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisMailConnectionFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
+    @Bean
+    public StatefulRedisConnection<String, String> connection(RedisClient redisClient) {
+        return redisClient.connect();
+    }
+
+    @Bean
+    public RedisCommands<String, String> redisCommands(StatefulRedisConnection<String, String> connection) {
+        return connection.sync();
     }
 }
