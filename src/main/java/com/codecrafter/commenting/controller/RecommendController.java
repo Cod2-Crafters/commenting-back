f3784f1cfc7404a08aa58166903b65edf87aaeb7
‘jayce8887@gmail.com’ <kjw88887@gmail.com>
2024-07-18 19:13:32 +0900

    add 좋은질문

--- src/main/java/com/codecrafter/commenting/controller/RecommendController.java
+++ src/main/java/com/codecrafter/commenting/controller/RecommendController.java
@@ -4,14 +4,19 @@ package com.codecrafter.commenting.controller;
 import com.codecrafter.commenting.domain.dto.ApiResponse;
 import com.codecrafter.commenting.domain.request.RecommendRequest;
 import com.codecrafter.commenting.domain.response.RecommendResponse;
+import com.codecrafter.commenting.domain.response.conversation.ConversationResponse;
 import com.codecrafter.commenting.service.RecommendService;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,10 +33,33 @@ public class RecommendController {
                         {host}/api/recommend/likes
                         
                         """)
-    @PostMapping("/like")
-    public ResponseEntity<ApiResponse> incrementLikes(@RequestBody RecommendRequest request) {
-        RecommendResponse response = recommendService.incrementLikes(request);
+    @PostMapping("/likes")
+    public ResponseEntity<ApiResponse> updateLikes(@RequestBody RecommendRequest request) {
+        RecommendResponse response = recommendService.updateLikes(request);
         return new ResponseEntity<>(ApiResponse.success(response), HttpStatus.OK);
+    }
+
+    @Operation(summary = "고마워요",
+        description = """
+                        ★고마워요 증/감</br>
+                        {host}/api/recommend/thanked
+                        
+                        """)
+    @PostMapping("/thanked")
+    public ResponseEntity<ApiResponse> updateThanked(@RequestBody RecommendRequest request) {
+        RecommendResponse response = recommendService.updateThanked(request);
+        return new ResponseEntity<>(ApiResponse.success(response), HttpStatus.OK);
+    }
+
+    @Operation(summary = "좋은질문들",
+        description = """
+                        ★내가 좋아요누른 대화 조회</br>
+                        {host}/api/recommends/user/{userId}
+                        """)
+        @PostMapping("/user")
+    public ResponseEntity<ApiResponse> getRecommendedConversations(@RequestHeader("Authorization") String token) {
+        List<ConversationResponse> conversations = recommendService.getRecommendedConversations(token);
+        return new ResponseEntity<>(ApiResponse.success(conversations), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/codecrafter/commenting/service/RecommendService.java
+++ src/main/java/com/codecrafter/commenting/service/RecommendService.java
@@ -1,15 +1,20 @@
 package com.codecrafter.commenting.service;
 
+import com.codecrafter.commenting.config.jwt.TokenProvider;
 import com.codecrafter.commenting.domain.entity.Conversation;
 import com.codecrafter.commenting.domain.entity.MemberInfo;
 import com.codecrafter.commenting.domain.entity.Recommend;
 import com.codecrafter.commenting.domain.enumeration.RecommendStatus;
 import com.codecrafter.commenting.domain.request.RecommendRequest;
 import com.codecrafter.commenting.domain.response.RecommendResponse;
+import com.codecrafter.commenting.domain.response.conversation.ConversationResponse;
 import com.codecrafter.commenting.repository.ConversationRepository;
 import com.codecrafter.commenting.repository.MemberInfoRepository;
 import com.codecrafter.commenting.repository.RecommendRepository;
+import io.jsonwebtoken.Claims;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,20 +28,23 @@ public class RecommendService {
     private final RecommendRepository recommendRepository;
     private final ConversationRepository conversationRepository;
     private final MemberInfoRepository memberInfoRepository;
+    private final TokenProvider tokenProvider;
     @Transactional
-    public RecommendResponse incrementLikes(RecommendRequest request) {
+    public RecommendResponse updateLikes(RecommendRequest request) {
         Conversation conversation = conversationRepository.findById(request.conversationId())
                                                             .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 대화입니다."));
         MemberInfo memberInfo = memberInfoRepository.findById(request.userId())
-                                                    .orElseThrow(() -> new IllegalArgumentException("로그인이 필요한 서비스입니다."));
+                                                            .orElseThrow(() -> new IllegalArgumentException("로그인이 필요한 서비스입니다."));
 
-        Optional<Recommend> existingRecommend = recommendRepository.findByConversationAndMemberInfo(conversation, memberInfo);
+        Optional<Recommend> existingRecommend = recommendRepository.findRecommend(conversation, memberInfo, RecommendStatus.LIKES);
 
-        // 좋아요 추가, 좋아요가 이미 있을 경우 제거
+        // 선택한 대화에 좋아요가 이미 있을 경우 제거
         if (existingRecommend.isPresent()) {
             recommendRepository.delete(existingRecommend.get());
             return new RecommendResponse(true, "delete");
-        } else {
+        }
+        // 선택한 대화에 좋아요
+        else {
             Recommend recommend = Recommend.builder()
                                             .conversation(conversation)
                                             .memberInfo(memberInfo)
@@ -46,4 +54,56 @@ public class RecommendService {
             return new RecommendResponse(true, "insert");
         }
     }
+
+    @Transactional
+    public RecommendResponse updateThanked(RecommendRequest request) {
+        // 익명추천관련해서 추후 구현시 좋아요와 로직 달리할 예정
+        Conversation conversation = conversationRepository.findById(request.conversationId())
+                                                            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 대화입니다."));
+        MemberInfo memberInfo = memberInfoRepository.findById(request.userId())
+                                                            .orElseThrow(() -> new IllegalArgumentException("로그인이 필요한 서비스입니다."));
+
+        Optional<Recommend> existingRecommend = recommendRepository.findRecommend(conversation, memberInfo, RecommendStatus.THANKED);
+
+        // 선택한 대화에 고마워요가 이미 있을 경우 제거
+        if (existingRecommend.isPresent()) {
+            recommendRepository.delete(existingRecommend.get());
+            return new RecommendResponse(true, "delete");
+        }
+        // 선택한 대화에 고마워요
+        else {
+            Recommend recommend = Recommend.builder()
+                                            .conversation(conversation)
+                                            .memberInfo(memberInfo)
+                                            .recommendStatus(RecommendStatus.THANKED)
+                                            .build();
+            recommendRepository.save(recommend);
+            return new RecommendResponse(true, "insert");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<ConversationResponse> getRecommendedConversations(String token) {
+        if (!tokenProvider.isTokenValid(token)) {
+            throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
+        }
+
+        Claims claims = tokenProvider.validateToken(token);
+        Long memberId = Long.parseLong(claims.getSubject());
+
+        List<Conversation> conversations = recommendRepository.findLikedConversationsByUserId(memberId, RecommendStatus.LIKES);
+
+        return conversations.stream()
+                            .map(conversation -> new ConversationResponse(
+                                    conversation.getId(),
+                                    conversation.getConversationMST().getOwner().getId(),
+                                    conversation.getConversationMST().getGuest() != null ? conversation.getConversationMST().getGuest().getId() : null,
+                                    conversation.getContent(),
+                                    conversation.isGood(),
+                                    conversation.isPrivate(),
+                                    conversation.isQuestion(),
+                                    conversation.getConversationMST().getId()))
+            .collect(Collectors.toList());
+    }
+
 }
