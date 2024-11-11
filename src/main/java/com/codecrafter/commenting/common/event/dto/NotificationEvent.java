24a8b64786794d5501ee137acbd4a692478a75a9
213kky <213kky@naver.com>
2024-11-11 21:34:27 +0900

    refactor: 명확한 변수명으로 수정 및 알림 타입 필드 추가

--- src/main/java/com/codecrafter/commenting/common/event/dto/NotificationEvent.java
+++ src/main/java/com/codecrafter/commenting/common/event/dto/NotificationEvent.java
@@ -2,18 +2,21 @@ package com.codecrafter.commenting.common.event.dto;
 
 import com.codecrafter.commenting.domain.entity.Conversation;
 import com.codecrafter.commenting.domain.entity.MemberInfo;
+import com.codecrafter.commenting.domain.enumeration.NotificationType;
 import lombok.Getter;
 
 @Getter
 public class NotificationEvent {
 
-    private MemberInfo guest;
-    private MemberInfo owner;
+    private MemberInfo sender;
+    private MemberInfo receiver;
     private Conversation conversation;
+    private NotificationType notificationType;
 
-    public NotificationEvent(MemberInfo guest, MemberInfo owner, Conversation conversation) {
-        this.guest = guest;
-        this.owner = owner;
+    public NotificationEvent(MemberInfo sender, MemberInfo receiver, Conversation conversation, NotificationType notificationType) {
+        this.sender = sender;
+        this.receiver = receiver;
         this.conversation = conversation;
+        this.notificationType = notificationType;
     }
 }
