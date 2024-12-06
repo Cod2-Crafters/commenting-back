a55d09b7ff9c5adae219c1b8e74b79d5dfca683f
213kky <213kky@naver.com>
2024-12-06 23:57:41 +0900

    refactor: 로그인 체크 인터셉터로 수정

--- src/main/java/com/codecrafter/commenting/config/WebServiceConfig.java
+++ src/main/java/com/codecrafter/commenting/config/WebServiceConfig.java
@@ -2,7 +2,7 @@ package com.codecrafter.commenting.config;
 
 import com.codecrafter.commenting.config.filter.ContentCachingFilter;
 import com.codecrafter.commenting.controller.interceptor.ConversationQuestionInterceptor;
-import com.codecrafter.commenting.controller.interceptor.MemberSettingInterceptor;
+import com.codecrafter.commenting.controller.interceptor.LoginCheckInterceptor;
 import com.codecrafter.commenting.controller.interceptor.RecommendInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -16,14 +16,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebServiceConfig implements WebMvcConfigurer {
 
-    private final MemberSettingInterceptor memberSettingInterceptor;
+    private final LoginCheckInterceptor loginCheckInterceptor;
     private final RecommendInterceptor recommendInterceptor;
     private final ConversationQuestionInterceptor conversationQuestionInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(memberSettingInterceptor)
-                .addPathPatterns("/api/member/setting/**");
+        registry.addInterceptor(loginCheckInterceptor)
+            .addPathPatterns(
+                "/api/member/setting/**", // 설정 수정
+                "/api/recommends/**",
+                "/api/conversations/question/update", // 질문수정
+                "/api/conversations/question/*", // 질문 삭제
+                "/api/conversations/answer", // 답변 작성
+                "/api/conversations/answer/update", // 답변 수정
+                "/api/conversations/answer/*", // 답변 삭제
+                "/api/conversations/members/*/send", // 보낸질문 조회
+                "/api/statistics" // 통계 조회
+            )
+
+        ;
 
         registry.addInterceptor(recommendInterceptor)
             .addPathPatterns("/api/recommends/**");
@@ -36,7 +48,8 @@ public class WebServiceConfig implements WebMvcConfigurer {
     public FilterRegistrationBean<ContentCachingFilter> contentCachingFilter() {
         FilterRegistrationBean<ContentCachingFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new ContentCachingFilter());
-        registrationBean.addUrlPatterns("/api/recommends/thanked", "/api/recommends/likes", "/api/conversations/question");
+        registrationBean.addUrlPatterns("/api/recommends/thanked", "/api/recommends/likes",
+            "/api/conversations/question");
         registrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
         return registrationBean;
     }
