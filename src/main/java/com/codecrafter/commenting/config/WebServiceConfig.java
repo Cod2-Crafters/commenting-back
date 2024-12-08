ba7ca0a7743be2510c7305ba27dafaa92b7a8aa8
213kky <213kky@naver.com>
2024-12-08 20:43:07 +0900

    refactor: 중복된 로그인 체크 로직 제거

--- src/main/java/com/codecrafter/commenting/config/WebServiceConfig.java
+++ src/main/java/com/codecrafter/commenting/config/WebServiceConfig.java
@@ -23,25 +23,23 @@ public class WebServiceConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor)
-            .addPathPatterns(
-                "/api/member/setting/**", // 설정 수정
-                "/api/recommends/**",
-                "/api/conversations/question/update", // 질문수정
-                "/api/conversations/question/*", // 질문 삭제
-                "/api/conversations/answer", // 답변 작성
-                "/api/conversations/answer/update", // 답변 수정
-                "/api/conversations/answer/*", // 답변 삭제
-                "/api/conversations/members/*/send", // 보낸질문 조회
-                "/api/statistics" // 통계 조회
-            )
-
-        ;
+                .addPathPatterns(
+                    "/api/member/setting/**", // 설정 수정
+                    "/api/recommends/**",
+                    "/api/conversations/question/update", // 질문수정
+                    "/api/conversations/question/*", // 질문 삭제
+                    "/api/conversations/answer", // 답변 작성
+                    "/api/conversations/answer/update", // 답변 수정
+                    "/api/conversations/answer/*", // 답변 삭제
+                    "/api/conversations/members/*/send", // 보낸질문 조회
+                    "/api/statistics" // 통계 조회
+                );
 
         registry.addInterceptor(recommendInterceptor)
-            .addPathPatterns("/api/recommends/**");
+                .addPathPatterns("/api/recommends/**");
 
         registry.addInterceptor(conversationQuestionInterceptor)
-            .addPathPatterns("/api/conversations/question");
+                .addPathPatterns("/api/conversations/question");
     }
 
     @Bean
